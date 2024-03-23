503fb323aed6fc9c8626e865727901be0eb01203
Ayesha-Ikram <160381040+Ayesha-Ikram@users.noreply.github.com>
2024-03-23 22:01:48 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+  
+const randomNumber =Math.floor(Math.random()*10);
+
+const game = await inquirer.prompt([
+    {
+        type:"number",
+        name:"guessnumber",
+        message:"Please Guess a Number between 1-10: ",
+    }
+])
+
+if(game.guessnumber === randomNumber){
+    console.log("Cogratulations! You guessed right number:)")
+}
+else{
+    console.log("Sorry! You guessed wrong number. ")
+}
